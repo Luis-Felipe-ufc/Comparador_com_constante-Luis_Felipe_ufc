b43057756faebac7771949976e037d7e87653d82
Luis-Felipe-ufc <luis-felipe.souza@acad.ufsm.br>
2024-11-13 10:06:44 -0300

    Update sistema_x.sv

--- Comparador_luis-felipe/sistema_x.sv
+++ Comparador_luis-felipe/sistema_x.sv
@@ -9,10 +9,10 @@ module sistema_x (Q, d, c, b, a);
   assign X = {a, b, c, d};
   assign B = 4'b0101;
   
-  XNOR2X1HVT NOR2_BIT_3( .A(X[3]), .B(B[3]), .Y(w1));
-  XNOR2X1HVT NOR2_BIT_2( .A(X[2]), .B(B[2]), .Y(w2));
-  XNOR2X1HVT NOR2_BIT_1( .A(X[1]), .B(B[1]), .Y(w3));
-  XNOR2X1HVT NOR2_BIT_0( .A(X[0]), .B(B[0]), .Y(w4));
+  XNOR2X1HVT XNOR2_BIT_3( .A(X[3]), .B(B[3]), .Y(w1));
+  XNOR2X1HVT XNOR2_BIT_2( .A(X[2]), .B(B[2]), .Y(w2));
+  XNOR2X1HVT XNOR2_BIT_1( .A(X[1]), .B(B[1]), .Y(w3));
+  XNOR2X1HVT XNOR2_BIT_0( .A(X[0]), .B(B[0]), .Y(w4));
   AND4X1HVT AND4( .A(w1), .B(w2), .C(w3), .D(w4), .Y(Q));
   
 endmodule
